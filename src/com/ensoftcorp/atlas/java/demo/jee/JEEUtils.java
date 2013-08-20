16295ede031ba61635d6cfca86ec0255da5f0cb7
Tom Deering <tomdeering7@gmail.com>
2013-08-20 16:35:25 -0500

    Added Java EE API specific selection detail scripts, for the purpose of JavaOne.

---
+++ src/com/ensoftcorp/atlas/java/demo/jee/JEEUtils.java
@@ -0,0 +1,216 @@
+package com.ensoftcorp.atlas.java.demo.jee;
+
+import static com.ensoftcorp.atlas.java.core.script.Common.index;
+import static com.ensoftcorp.atlas.java.core.script.Common.toQ;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+
+import com.ensoftcorp.atlas.java.core.db.graph.GraphElement;
+import com.ensoftcorp.atlas.java.core.db.view.View;
+import com.ensoftcorp.atlas.java.core.index.common.SourceCorrespondence;
+import com.ensoftcorp.atlas.java.core.index.common.WorkspaceResourceNotFoundException;
+import com.ensoftcorp.atlas.java.core.index.summary.SummaryGraph.SummaryNode;
+import com.ensoftcorp.atlas.java.core.query.Attr.Edge;
+import com.ensoftcorp.atlas.java.core.query.Attr.Node;
+import com.ensoftcorp.atlas.java.core.query.Q;
+import com.ensoftcorp.atlas.java.core.script.UniverseManipulator;
+import com.ensoftcorp.atlas.java.core.script.UniverseManipulator.Manipulation;
+
+public class JEEUtils {
+	public static final String FILE = "FILE";
+	public static final String NON_JAVA_STRING_REFERENCE = "NON_JAVA_STRING_REFERENCE";
+	
+	/**
+	 * 
+	 * @param projectName
+	 * @param toFind
+	 * @return
+	 */
+	@SuppressWarnings("serial")
+	public static Q getRawStringReferences(String projectName, String matchRegex, String notMatchRegex, String... fileExtensions){
+		// Get all files in the given project with one of the given extensions
+		List<File> files = projectFilesMatchingExtension(projectName, fileExtensions);
+		
+		View v = new View();
+		UniverseManipulator um = new UniverseManipulator();
+		
+		GraphElement decEdge = index().edgesTaggedWithAny(Edge.DECLARES).eval().edges().getFirst();
+		
+		// For each file
+		for(final File file : files){
+			// Read in the lines of the file
+			List<Line> lines = readLines(file);
+			
+			// Get the lines which match the regex
+			List<Line> matches = linesMatchingRegex(lines, matchRegex, notMatchRegex);
+			
+			// If at least one line matches the regex, add stuff to the view.
+			if(matches.size() > 0){
+				final IFile iFile = fileToIFile(file);
+				
+				Set<String> tags = new HashSet<String>(){{
+					add(FILE);
+				}};
+				
+				Map<String, Object> attributes;
+				SourceCorrespondence sc = null;
+				try {
+					sc = SourceCorrespondence.fromString("0", Long.toString(file.length()), iFile);
+				} catch (WorkspaceResourceNotFoundException e) {}
+				final SourceCorrespondence sc2 = sc;
+				attributes = new HashMap<String, Object>(){{
+					put(Node.NAME, file.getName().trim());
+					put(SummaryNode.SC_RANGE, sc2);
+				}};
+				
+				Manipulation fileNode = um.createNode(tags, attributes);
+				um.addToView(fileNode, v);
+				
+				tags = new HashSet<String>(){{
+					add(NON_JAVA_STRING_REFERENCE);
+				}};
+				for(final Line line : matches){
+					try {
+						sc = SourceCorrespondence.fromString(Integer.toString(line.getStartOffset()), 
+															 Integer.toString(line.getEndOffset()),
+															 iFile);
+					} catch (WorkspaceResourceNotFoundException e) {}
+					
+					final SourceCorrespondence sc3 = sc;
+					attributes = new HashMap<String, Object>(){{
+						put(Node.NAME, line.getLine().trim());
+						put(SummaryNode.SC_RANGE, sc3);
+					}};
+					
+					Manipulation lineNode = um.createNode(tags, attributes);
+					Manipulation lineDeclaration = um.createEdge(decEdge, fileNode, lineNode);
+					um.addToView(lineNode, v);
+					um.addToView(lineDeclaration, v);
+				}
+			}
+		}
+
+		um.perform();
+		return toQ(v);
+	}
+	
+	private static IFile fileToIFile(File file){
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();    
+		IPath location = Path.fromOSString(file.getAbsolutePath()); 
+		IFile iFile = workspace.getRoot().getFileForLocation(location);
+		return iFile;
+	}
+	
+	private static List<Line> linesMatchingRegex(List<Line> lines, String matchRegex, String notMatchRegex){
+		List<Line> matches = new LinkedList<Line>();
+		
+		for(Line line : lines){
+			if(line.getLine().matches(matchRegex) && 
+			   !line.getLine().matches(notMatchRegex)) matches.add(line);
+		}
+		
+		return matches;
+	}
+	
+	private static List<Line> readLines(File f){
+		List<Line> lines = new LinkedList<Line>();
+
+		int startOffset = 0;
+		int endOffset = 0;
+		BufferedReader bfr = null;
+		try {
+			bfr = new BufferedReader(new FileReader(f));
+			String line;
+			while((line = bfr.readLine()) != null) {
+				endOffset = startOffset + line.length();
+				lines.add(new Line(line, startOffset, endOffset)); 
+				startOffset = endOffset + 1;
+			}
+
+		} 
+		catch (FileNotFoundException e) {} 
+		catch (IOException e) {}
+		finally{
+			if(bfr != null)
+				try {
+					bfr.close();
+				} catch (IOException e) {}
+		}
+		
+		return lines;
+	}
+	
+	private static List<File> projectFilesMatchingExtension(String projectName, String... fileExtensions){
+		List<File> res = new LinkedList<File>();
+		
+		Set<String> extensionSet = new HashSet<String>(fileExtensions.length * 2);
+		for(String extension : fileExtensions) extensionSet.add(extension);
+		
+		// If the project has a src folder, use that. Else use the project root.
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		File root = project.getLocation().toFile();
+		for(File child : root.listFiles()){
+			if(child.isDirectory() && child.getName().equalsIgnoreCase("src")){
+				root = child;
+				break;
+			}
+		}
+		
+		projectFilesMatchingExtension(root, res, extensionSet);
+		
+		return res;
+	}
+	
+	private static void projectFilesMatchingExtension(File current, List<File> list, Set<String> fileExtensions){
+		if(current.isDirectory()){
+			File[] containedFiles = current.listFiles();
+			for(File f : containedFiles) projectFilesMatchingExtension(f, list, fileExtensions);
+		} else if (current.exists()){
+			String[] split = current.getName().split("\\.");
+			if(split.length > 0 && fileExtensions.contains(split[split.length - 1])){
+				list.add(current);
+			}
+		}
+	}
+	
+	private static class Line{
+		private String line;
+		private int startOffset, endOffset;
+		public Line(String line, int startOffset, int endOffset) {
+			super();
+			this.line = line;
+			this.startOffset = startOffset;
+			this.endOffset = endOffset;
+		}
+		public String getLine() {
+			return line;
+		}
+		public int getStartOffset() {
+			return startOffset;
+		}
+		public int getEndOffset() {
+			return endOffset;
+		}
+	}
+}
